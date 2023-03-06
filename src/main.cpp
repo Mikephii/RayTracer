d22b8e18dc555fc3726fedbd826bd6617761005a
Mike Collins <mich.collins1@gmail.com>
2023-03-06 22:54:06 +1100

    init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <zlib.h>
+#include <png++/png.hpp>
+int main(){
+    std::cout<<"hello world";
+   
+    png::image< png::rgb_pixel > image(720.f, 560.f);
+    for (float y = 0; y < image.get_height(); ++y) {
+
+        for (float x = 0; x < image.get_width(); ++x) {
+            
+            image[y][x] = png::rgb_pixel(x/image.get_width()*255.f, y/image.get_height()*255.f, (x/y)*255.f);
+            // non-checking equivalent of image.set_pixel(x, y, ...);
+        }
+    }
+    image.write("rgb.png");
+    return 0;
+}
